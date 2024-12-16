23bb828e07289971906ee64acc8f17863ab9eaa7
Dominick Randazzo <143025079+dazzo5@users.noreply.github.com>
2024-12-15 19:12:46 -0500

    added prework files to repo

---
+++ codepathios101preworkTests/codepathios101preworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  codepathios101preworkTests.swift
+//  codepathios101preworkTests
+//
+//  Created by Dominick Randazzo on 12/12/24.
+//
+
+import Testing
+@testable import codepathios101prework
+
+struct codepathios101preworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ codepathios101preworkUITests/codepathios101preworkUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  codepathios101preworkUITestsLaunchTests.swift
+//  codepathios101preworkUITests
+//
+//  Created by Dominick Randazzo on 12/12/24.
+//
+
+import XCTest
+
+final class codepathios101preworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
